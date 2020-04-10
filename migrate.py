92fcc7b5c33b78377ab1dcdc06eb2754913ed7c1
Максимов Константин <kmaksimovv@gmail.com>
2020-04-10 12:02:19 +0400

    scan ip

--- migrate.py
+++ migrate.py
@@ -5,12 +5,13 @@ import sqlite3
 conn = sqlite3.connect("db_scan_ip.db") 
 cursor = conn.cursor()
 
-sql_create_scan_ip_table = """ CREATE TABLE IF NOT EXISTS scan_ip (
-                                        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                        `ip` varchar(25),
-                                        `port` varchar(25),
-                                        `state` varchar(25)
-                                        )"""
+# sql_create_scan_ip_table = """ CREATE TABLE IF NOT EXISTS scan_ip (
+#                                         `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+#                                         `ip` varchar(25),
+#                                         `port` varchar(25),
+#                                         `status` varchar(25)
+#                                         )"""
 
-cursor.execute(sql_create_scan_ip_table)
-                                    
+# cursor.execute(sql_create_scan_ip_table)
+
+
--- scan_ip.py
+++ scan_ip.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3.6
 import nmap
+import sqlite3
 
 file_ip_list = open("ip_list.txt","r")
 
+insert_sql_query = """INSERT INTO scan_ip (ip,port,status) VALUES (?, ?, ?)"""
+select_sql_query = """SELECT COUNT(*) from scan_ip where ip = ? and port = ?"""
+
+conn = sqlite3.connect("db_scan_ip.db") 
+cursor = conn.cursor()
+
+
 nm = nmap.PortScanner()
 
-# with file_ip_list as data:
-#     for line in data:
-#         line = line.strip()
-#         nm.scan(line, arguments='-sV -p 1-65535 -Pn')
-#         print(line)
-#         print('----------------------------------------------------')
-#         print('Host : %s (%s)' % (line, nm[line].hostname()))
-#         print('State : %s' % nm[line].state())
-#         for proto in nm[line].all_protocols():
-#             print('----------')
-#             print('Protocol : %s' % proto)
+with file_ip_list as data:
+    for line in data:
+        line = line.strip()
+        nm.scan(line, arguments='-sV -p 1-65535 -Pn')
+        print(line)
+        print('----------------------------------------------------')
+        print('Host : %s (%s)' % (line, nm[line].hostname()))
+        print('State : %s' % nm[line].state())
+        for proto in nm[line].all_protocols():
+            print('----------')
+            print('Protocol : %s' % proto)
     
-#             lport = nm[line][proto].keys()
+            lport = nm[line][proto].keys()
 
-#             for port in lport:
-#                 print ('port : %s\tstate : %s' % (port, nm[line][proto][port]['state']))
+            for port in lport:
+                print ('port : %s\tstate : %s' % (port, nm[line][proto][port]['state']))
+                
+                data_select = [line, port]
+                count = cursor.execute(select_sql_query, data_select)
+                
+                print('Count: %s' % (count.fetchall())
+                if count.fetchone() == 0:
+                    print("ravno!!!!!!!")    
+                    data_insert = [line, port, nm[line][proto][port]['state']]
+                    cursor.execute(insert_sql_query, data_insert)
+conn.commit()
+conn.close()
