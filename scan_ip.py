a3c458f7d774f34e5bdb814242c2aef514d203bb
Maksimov Konstantin <kmakimovv@gmail.com>
2020-04-10 15:23:04 +0400

    add hosts to file

--- scan_ip.py
+++ scan_ip.py
@@ -1,42 +1,48 @@
-#!/usr/bin/python3.6
+#!/usr/bin/env python3.6
+
 import nmap
 import sqlite3
 
 file_ip_list = open("ip_list.txt","r")
 
 insert_sql_query = """INSERT INTO scan_ip (ip,port,status) VALUES (?, ?, ?)"""
-select_sql_query = """SELECT COUNT(*) from scan_ip where ip = ? and port = ?"""
+select_sql_query_count = """SELECT COUNT(*) from scan_ip where ip = ? and port = ?"""
+select_sql_query_status_port = """select status from scan_ip WHERE ip = ? and port = ?"""
+update_sql_query_status_port = """UPDATE scan_ip SET status = ?  where ip = ? and port = ?"""
 
 conn = sqlite3.connect("db_scan_ip.db") 
 cursor = conn.cursor()
 
-
 nm = nmap.PortScanner()
 
 with file_ip_list as data:
-    for line in data:
-        line = line.strip()
-        nm.scan(line, arguments='-sV -p 1-65535 -Pn')
-        print(line)
-        print('----------------------------------------------------')
-        print('Host : %s (%s)' % (line, nm[line].hostname()))
-        print('State : %s' % nm[line].state())
-        for proto in nm[line].all_protocols():
-            print('----------')
-            print('Protocol : %s' % proto)
-    
-            lport = nm[line][proto].keys()
+    for ip in data:
+        ip = ip.strip()
+        # nm.scan(ip, arguments='-sV -p 1-65535 -Pn')
+        nm.scan(ip, arguments='-sV -p 22,80 -Pn')
+        for proto in nm[ip].all_protocols():
+            lport = nm[ip][proto].keys()
 
             for port in lport:
-                print ('port : %s\tstate : %s' % (port, nm[line][proto][port]['state']))
+                status_port = nm[ip][proto][port]['state']
+                data_select = [ip, port]
+
+                count = cursor.execute(select_sql_query_count, data_select)
+                data_insert = [ip, port, status_port]
+                data_select_port = [ip, port]
+                count = count.fetchone()[0]
                 
-                data_select = [line, port]
-                count = cursor.execute(select_sql_query, data_select)
-                
-                print('Count: %s' % (count.fetchall())
-                if count.fetchone() == 0:
-                    print("ravno!!!!!!!")    
-                    data_insert = [line, port, nm[line][proto][port]['state']]
+                if count == 0:
                     cursor.execute(insert_sql_query, data_insert)
+                else:
+                    cursor.execute(select_sql_query_status_port, data_select_port)
+                    status_port_sql = cursor.fetchone()[0]
+
+                    if status_port_sql != status_port:
+                        data_update = [status_port, ip, port]
+                        cursor.execute(update_sql_query_status_port, data_update)
+                        with open('scan_hosts.txt', 'a') as f:
+                            f.write(f"ip: {ip}, port: {port}, status: {status_port}\n")
+
 conn.commit()
 conn.close()
--- sql_connect_db.py
+++ sql_connect_db.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3.6
+#!/usr/bin/env python3.6
+
 import sqlite3
 conn = sqlite3.connect("db_scan_ip.db") 
 cursor = conn.cursor()
